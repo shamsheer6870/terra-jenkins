a8c16751f5ec416e3a7c13064b48cced9618bd3e
shamsheer <shamsheer@ubuntu.localdomain>
2026-03-28 10:06:00 +0530

    first-commit terra-script vpc create

---
+++ main.tf
@@ -0,0 +1,19 @@
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 5.0"
+    }
+  }
+}
+
+provider "aws" {
+  region = "us-east-1"
+}
+
+resource "aws_vpc" "jenkins_demo_vpc" {
+  cidr_block = "10.0.0.0/16"
+  tags = {
+    Name = "Jenkins-Git-Terraform-Demo"
+  }
+}
